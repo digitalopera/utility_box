ae2fa805e41aaf88eaf148e767e0b6dc4f879550
JD Hendrickson <jd@digitalopera.com>
2013-01-10 22:54:43 -0600

    updated requires

--- app/assets/javascripts/utility_box.js
+++ app/assets/javascripts/utility_box.js
@@ -1,9 +1,11 @@
-//= require utility_box/namespace
-//= require utility_box/connection_manager
-//= require utility_box/ellipsis
-//= require utility_box/events
-//= require utility_box/forms
-//= require utility_box/preload_images
-//= require utility_box/pusher
-//= require utility_box/strings
-//= require utility_box/tables
+//= require ./utility_box/namespace
+//= require ./utility_box/connection_manager
+//= require ./utility_box/events
+//= require ./utility_box/forms
+//= require ./utility_box/preload_images
+//= require ./utility_box/pusher
+//= require ./utility_box/strings
+//= require ./utility_box/tables
+
+//= require_tree ./utility_box/animation
+//= require_tree ./utility_box/storage
